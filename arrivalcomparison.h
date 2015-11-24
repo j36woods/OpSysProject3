476a676f3e53c31dcd610884f8c8730eac0d75ac
Jason Woods <j36woods@gmail.com>
2015-11-24 17:16:00 -0500

    Added secondary comparison to keep input order consistent if arrival times are equal

--- arrivalcomparison.h
+++ arrivalcomparison.h
@@ -6,6 +6,9 @@
 class ArrivalComparison {
 public:
 	bool operator()(const Process* lhs, const Process* rhs){
+		if (lhs->get_arrival_time() == rhs->get_arrival_time()) {
+			return lhs->get_proc_arrival_num() > rhs->get_proc_arrival_num();
+		}
 		return lhs->get_arrival_time() > rhs->get_arrival_time();
 	}
 };
